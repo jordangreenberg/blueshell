567ea497803b2f151771fb45d255612355053f72
Jordan <jordan.greenberg@mail.utoronto.ca>
2022-11-13 13:27:48 -0500

    Implement matlab logic up to and including condition where all 4 directions are safe

--- bluebot/motors.ino
+++ bluebot/motors.ino
@@ -1,4 +1,7 @@
 #define DEFAULT_SPEED 40
+#define ROTATE_SPEED 60
+#define ROTATE_TIME 250
+
 // MOTOR CONTROLLER FUNCTIONS /////////////////////////////////
 // Code adapted from MC_PWM_clean.ino provided on Quercus
 // Potentially for Uno and not Mega, will have to test
@@ -11,6 +14,12 @@ void set_speed(int motor_en, int desired_speed)
   {
     analogWrite(motor_en, desired_speed);
   }
+}
+
+void drive()
+{
+  set_speed(*right_en, *rightSpeed);
+  set_speed(*left_en, *leftSpeed);
 }
 
 // Change direction of a motor given the motor input pins
@@ -27,6 +36,35 @@ void change_direction(int motor_forward, int motor_in1, int motor_in2)
     digitalWrite(motor_in1, LOW);
     digitalWrite(motor_in2, HIGH);
   }
+}
+
+// Stops all motors
+void brake() {
+  set_speed(motor1_en, 0);
+  set_speed(motor2_en, 0);
+  set_speed(motor3_en, 0);
+  set_speed(motor4_en, 0);
+}
+
+// Function to rotate the robot in baby steps (ideally 2 degrees at a time)
+void rotate()
+{
+  // Set motor directions to rotate clockwise
+  change_direction(false, motor1_in1, motor1_in2);
+  change_direction(true, motor2_in1, motor2_in2);
+  change_direction(true, motor3_in1, motor3_in2);
+  change_direction(false, motor4_in1, motor4_in2);
+
+  // Set rotate speed
+  set_speed(motor1_en, ROTATE_SPEED);
+  set_speed(motor2_en, ROTATE_SPEED);
+  set_speed(motor3_en, ROTATE_SPEED);
+  set_speed(motor4_en, ROTATE_SPEED);
+
+  delay(ROTATE_TIME);
+
+  // Set rotate speed
+  brake();
 }
 
 // TODO: Check this function to make sure motor assignments/sensors match real life
@@ -53,6 +91,10 @@ void change_heading(int forwardMotor)
     // Assign right/left distances
     rightDistance = &distance4;
     leftDistance = &distance2;
+    prevRight = &prevDistance4;
+    prevLeft = &prevDistance2;
+    forwardDistance = &distance1;
+    backDistance = &distance3;
   }
   else if (forwardMotor == 2)
   {
@@ -69,6 +111,10 @@ void change_heading(int forwardMotor)
     // Assign right/left distances
     rightDistance = &distance1;
     leftDistance = &distance3;
+    prevRight = &prevDistance1;
+    prevLeft = &prevDistance3;
+    forwardDistance = &distance2;
+    backDistance = &distance4;
   }
   else if (forwardMotor == 3)
   {
@@ -85,6 +131,10 @@ void change_heading(int forwardMotor)
     // Assign right/left distances
     rightDistance = &distance2;
     leftDistance = &distance4;
+    prevRight = &prevDistance2;
+    prevLeft = &prevDistance4;
+    forwardDistance = &distance3;
+    backDistance = &distance1;
   }
   else if (forwardMotor == 4)
   {
@@ -101,11 +151,50 @@ void change_heading(int forwardMotor)
     // Assign right/left distances
     rightDistance = &distance3;
     leftDistance = &distance1;
+    prevRight = &prevDistance3;
+    prevLeft = &prevDistance1;
+    forwardDistance = &distance4;
+    backDistance = &distance2;
   }
 
   // Set forward motors to default speed
   *rightSpeed = DEFAULT_SPEED;
   *leftSpeed = DEFAULT_SPEED;
+}
+
+int reverseHeading(int forwardMotor) {
+  if (forwardMotor == 1) {
+    return 3;
+  }
+  else if (forwardMotor == 2) {
+    return 4;
+  }
+  else if (forwardMotor == 3) {
+    return 1;
+  }
+  else {
+    return 2;
+  }
+}
+
+int rotateHeading(int rotateDirection, int forwardMotor) {
+  int newHeading;
+  if (rotateDirection == RIGHT) {
+    newHeading = forwardMotor - 1;
+  }
+  else {
+    newHeading = forwardMotor + 1;
+  }
+
+  // Make sure newHeading is within bounds of 1-4
+  if (newHeading < 1) {
+    newHeading = 4;
+  }
+  else if (newHeading > 4) {
+    newHeading = 1;
+  }
+
+  return newHeading;
 }
 
 void constrain_speeds()
@@ -141,11 +230,5 @@ void adjustSpeeds(double correction)
     *rightSpeed -= (int) correction;
   }
   constrain_speeds();
-}
-
-void drive()
-{
-  set_speed(*right_en, *rightSpeed);
-  set_speed(*left_en, *leftSpeed);
 }
 // END OF MOTOR CONTROLLER FUNCTIONS //////////////////////////
