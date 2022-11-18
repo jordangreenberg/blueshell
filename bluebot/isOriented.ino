f8a60f207d19eb057ddbea8dbe868f1349d9e11b
abisuku <abhishek.sukumar@gmail.com>
2022-11-18 14:53:00 -0500

    v1 with delay

--- bluebot/isOriented.ino
+++ bluebot/isOriented.ino
@@ -6,11 +6,15 @@ void isOriented (float sensor5_reading, float sensor1_reading){
   while (oriented == false){
     if (sensor5_reading < (1-tolerance_orient)*sensor1_reading){ // checks if sensors are reading similar values, if not, rotate until they are within the tolerance
       rotate_counter_clockwise();
+      delay (250);
+      brake();
       if sensor5_reading >(1-tolerance_orient) * sensor1_reading && sensor5_reading < (1 + tolerance_orient) * sensor1_reading{
         oriented = true;
       }
     if (sensor1_reading < (1-tolerance_orient)*sensor5_reading){
       rotate_clockwise();
+      delay (250);
+      brake()
       if sensor1_reading >(1-tolerance_orient) * sensor5_reading && sensor1_reading < (1 + tolerance_orient) * sensor5_reading{
         oriented = true;
       }
